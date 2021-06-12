c612ea9aace8114268369f0acb623b2065bbe1ff
mariakiraga <marysia.kiraga@gmail.com>
2021-06-12 13:39:03 +0200

    reading from file + dialog window + saving data

--- dane.py
+++ dane.py
@@ -11,8 +11,9 @@ from psychopy import visual, event, logging, gui, core
 N_TRIALS_TRAIN = 1
 N_TRAILS_EXP = 4
 REACTION_KEYS = ['q', 'p']
-RESULTS = [["TRIAL", "TRAINING", "TRIAL_TYPE", "REACTION", "CORRECT", "CONGRUENT","LATENCY"]]
-#"ID","SEX","AGE",
+RESULTS = [["ID", 'SEX', 'AGE', "TRIAL", "TRAINING", "CORRECT", "CONGRUENT","LATENCY"]]
+#RESULTS = [["ID", "TRIAL", "TRAINING", "CORRECT", "CONGRUENT","LATENCY"]]
+
 
 def reactions(keys):
     event.clearEvents()
@@ -34,13 +35,10 @@ def read_text_from_file(file_name, insert=''):
 def show_info(win, file_name, insert=''):
     msg = read_text_from_file(file_name, insert=insert)
     msg = visual.TextStim(win, color='black', text=msg,
-                          height=20)
+                          height=40)
     msg.draw()
     win.flip()
-    key = event.waitKeys(keyList=[chr(27), 'space'])
-   # if key == [chr(27)]:
-   #     abort_with_error(
-    #        'Experiment finished by user on info screen! Esc pressed.')
+    key = event.waitKeys(keyList=["Esc", 'space'])
     win.flip()
 
 
@@ -71,13 +69,37 @@ def part_of_experiment(n_trials, train, fix, time):
             print(corr)
 
 
-        RESULTS.append([i+1, train, corr, con, rt])
+        RESULTS.append([id, sex, age, i+1, train, corr, con, rt])
+        #RESULTS.append([ID, i+1, train, corr, con, rt])
 
-
-window = visual.Window(units="pix", color="gray", fullscr=False, size=(1500, 1500))
-window.setMouseVisible(False)
 
 clock = core.Clock()
+
+# DIALOG BOX
+
+window = visual.Window(units="pix", color="gray", fullscr=False, size=(1500, 1500))
+window.setMouseVisible(True)
+
+info={'ID':'', 'PLEC': ['M', 'K'], 'WIEK':''}
+dlg = gui.DlgFromDict(info, title='Badanie efektu Simona. Wpisz swoje dane :)')
+if not dlg.OK:
+    print("User exited")
+    core.quit()
+
+id = info['ID']
+sex = info['PLEC']
+age = info['WIEK']
+#ID = info['ID'] + info['PLEC'] + info['WIEK']
+
+datafile = '{}{}{}_data.csv'.format(info['ID'], info['PLEC'], info['WIEK'])
+#datafile = 'ID.csv'
+def save_data():
+    with open(join('results', datafile), "w", newline='') as df:
+        write = csv.writer(df)
+        write.writerows(RESULTS)
+
+window = visual.Window(units="pix", color="gray", fullscr=True, size=(1500, 1500))
+window.setMouseVisible(False)
 
 stim = {"left_com": visual.TextStim(win=window, text="LEWO", color="red", pos=(-500.0, 0.0), height=80),
         "left_incom": visual.TextStim(win=window, text="LEWO", color="red", pos=(500.0,0.0), height=80),
@@ -85,11 +107,6 @@ stim = {"left_com": visual.TextStim(win=window, text="LEWO", color="red", pos=(-
         "right_incom": visual.TextStim(win=window, text="PRAWO", color="red", pos=(-500.0, 0.0), height=80)}
 
 fix = visual.TextStim(win=window, text="+", color="black", height=60)
-
-inst1 = visual.TextStim(win=window, text="instrukcja", color="white", height=40)
-inst2 = visual.TextStim(win=window, text="teraz eksperyment", color="white", height=40)
-inst_end = visual.TextStim(win=window, text="koniec", color="white", height=40)
-
 
 # TRAINING
 show_info(window, join('.', 'messages', 'train_mess.txt'))
@@ -99,14 +116,8 @@ part_of_experiment(N_TRIALS_TRAIN, train=True, fix=fix, time=1)
 show_info(window, join('.', 'messages', 'exp_mess.txt'))
 part_of_experiment(N_TRAILS_EXP, train=False, fix=fix, time=1)
 
+save_data()
+
 # THE END
 show_info(window, join('.', 'messages', 'fin_mess.txt'))
-
-with open("result.csv", "w", newline='') as f:
-    write = csv.writer(f)
-    write.writerows(RESULTS)
-
-
-
-
-
+window.close()
