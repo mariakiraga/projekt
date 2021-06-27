1a00b0d412a3f51b88508446b7d3e121dd498058
ZSams <zosiasamsel@gmail.com>
2021-06-27 12:23:20 +0200

    kamilślimak

--- main.py
+++ main.py
@@ -76,22 +76,21 @@ def save_data():
         write.writerows(RESULTS)
 
 
-def run_trial(win,):
+def run_trial(win):
     """
     Opis jednego trialu.
     :param win:
-    :param n_trials: liczba powtórzen
     :return:
     """
     global key, rt, con, corr, stim_type, prev_stim
 
-#losowanie bodzca tak, ze nie ma dwóch takich samych po sobie
+    # losowanie bodzca tak, ze nie ma dwóch takich samych po sobie
     stim_type = random.choice(list(stim.keys()))
     while stim_type == prev_stim:
         stim_type = random.choice(list(stim.keys()))
     prev_stim = stim_type
 
-   # punkt fiksacji
+    # punkt fiksacji
     fix.setAutoDraw(True)
     win.flip()
     core.wait(conf['FIX_CROSS_TIME'])  # wyswietlanie samego punktu fiksacji
@@ -109,18 +108,17 @@ def run_trial(win,):
         k = event.getKeys(conf['REACTION_KEYS'])
         if k == ['q'] or k == ['p']:
             rt = clock.getTime()
-            win.flip()
+            #win.flip()
             break
         if k == ['g']:
             win.close()
             core.quit()
-        win.flip()
+        #win.flip()
 
     key = k
-    print(key)
     if clock.getTime() > conf['TIME_MAX']:
         rt = '-'
-        win.flip()
+        #win.flip()
 
 
     stim[stim_type].setAutoDraw(False)
@@ -145,8 +143,7 @@ def run_trial(win,):
         con = 1
     elif (stim_type == "left_incom") or stim_type == "right_incom":
         con = 0
-    else:
-        con = "-"
+
 
     RESULTS.append([ID, trial_no, train, corr, con, rt])
 
@@ -181,13 +178,13 @@ window.setMouseVisible(False)
 # bodzce
 fix = visual.TextStim(win=window, text="+", color=conf['FIX_CROSS_COLOR'], height=conf['FIX_CROSS_SIZE'])
 
-stim = {"left_com":visual.TextStim(win=window, text="LEWO", height=conf['STIM_SIZE'],
+stim = {"left_com": visual.TextStim(win=window, text="LEWO", height=conf['STIM_SIZE'],
                                      color=conf['STIM_COLOR'], pos=(-500.0, 0.0)),
-            "left_incom":visual.TextStim(win=window, text="LEWO", height=conf['STIM_SIZE'],
+            "left_incom": visual.TextStim(win=window, text="LEWO", height=conf['STIM_SIZE'],
                                        color=conf['STIM_COLOR'], pos=(500.0, 0.0)),
-            "right_com":visual.TextStim(win=window, text="PRAWO", height=conf['STIM_SIZE'],
+            "right_com": visual.TextStim(win=window, text="PRAWO", height=conf['STIM_SIZE'],
                                       color=conf['STIM_COLOR'], pos=(500.0, 0.0)),
-            "right_incom":visual.TextStim(win=window, text="PRAWO", height=conf['STIM_SIZE'],
+            "right_incom": visual.TextStim(win=window, text="PRAWO", height=conf['STIM_SIZE'],
                                         color=conf['STIM_COLOR'], pos=(-500.0, 0.0))}
 
 # informacje o eksperymencie, instrukcje
@@ -203,12 +200,9 @@ for block_no in range(conf['NO_BLOCK_TRAIN']):
     for a in range(conf['N_TRIALS_TRAIN']):
         if a == 0:
             prev_stim = '0'
-        print(prev_stim)
-        trial_no = a
-        trial_no += 1
+        trial_no = a + 1
         train = 1
         run_trial(window)
-
     window.flip()
 
 # eksperyment
@@ -218,9 +212,7 @@ for block_no in range(conf['NO_BLOCK_EXP']):
     for i in range(conf['N_TRIALS_EXP']):
         if i == 0:
             prev_stim = '0'
-        print(prev_stim)
-        trial_no = i
-        trial_no += 1
+        trial_no = i + 1
         train = 0
         run_trial(window)
 
